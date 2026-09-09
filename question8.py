d708dc691edbd46e11ca79ad50b64795e5db0a2a
ANAYAPATIL7100 <anayaavinashpatil7100@gmail.com>
2026-09-09 19:09:23 +0530

    Delete question8.py

--- question8.py
+++
@@ -1,6 +0,0 @@
-numerator=int(input("Enter  numerator:"))
-denominator=int(input("Enter2 denominator:"))
-quotient=numerator//denominator
-remainder=numerator%denominator
-result=(quotient, remainder)
-print(result)
