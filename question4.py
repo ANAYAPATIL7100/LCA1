d92928d6fb89a985a7b6aa7011b05fcc9cc997f9
ANAYAPATIL7100 <anayaavinashpatil7100@gmail.com>
2026-09-09 19:08:29 +0530

    Delete question4.py

--- question4.py
+++
@@ -1,5 +0,0 @@
-my_dict={'name':'riya','roll number':15,'branch':'CSE','age':20,'university':'MIT-WPU'}
-keys_list=list(my_dict)
-print(keys_list)  #['name', 'roll number', 'branch', 'age', 'university']
-list2=list(my_dict.values())
-print(list2)  #['riya', 15, 'CSE', 20, 'MIT-WPU']
