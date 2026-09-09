7d81048685632d37adb8905883c8d8c43f646e4c
ANAYAPATIL7100 <anayaavinashpatil7100@gmail.com>
2026-09-09 21:33:32 +0530

    Add files via upload

---
+++ assignment 01.py
@@ -0,0 +1,21 @@
+Dic = {
+    "name": "Anaya",
+    "Age": 18,
+    "PRN": 1272261969,
+    "branch": "CS",
+}
+print(Dic)
+Dic["email"] = "anaya@example.com"
+print(Dic)  
+
+Dic.pop("Age")
+print(Dic)
+
+Dic["python programming"] = ""  # This is the correct way to add a new key-value pair to a dictionary
+print(Dic)
+
+tuople1 = ("apple", "banana", "cherry")
+print(tuople1)
+
+tuple = (1,2,3,4,5,6,7,8,9,10)
+print(tuple[1:5])  # Slicing the tuple to get elements from index 1 to 4
